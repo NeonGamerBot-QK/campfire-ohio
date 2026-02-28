bffc65d7f0ae3b69c6e5e7542372b7e6d2b726f9
tomaetto <158106199+tomaetto@users.noreply.github.com>
2026-02-28 15:01:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Water Space Platformer")
 clock = pygame.time.Clock()
-game = Game(clock)
 
-game.setup(screen)
 
 while game.running:
     for event in pygame.event.get():
