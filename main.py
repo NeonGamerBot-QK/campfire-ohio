c3195c9871e89d3a724668b350495042d982598e
tomaetto <158106199+tomaetto@users.noreply.github.com>
2026-02-28 14:56:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Game import Game
 import pygame
+import menu
 
 pygame.init()
 
@@ -10,7 +11,8 @@ pygame.display.set_caption("Water Space Platformer")
 clock = pygame.time.Clock()
 game = Game(clock)
 
-game.setup(screen)
+
+menu.is_enabled()
 
 while game.running:
     for event in pygame.event.get():
