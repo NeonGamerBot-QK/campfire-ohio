67aecc038f4bd7adb72b48416f8d6a8458a87688
tomaetto <158106199+tomaetto@users.noreply.github.com>
2026-03-01 14:49:59 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,14 +1,18 @@
 import pygame
 import sys
+import Player
 
 class SimpleMenu:
-    def __init__(self, screen, on_play):
+    def __init__(self, screen, on_play, game):
+        
+        self.game = game
         self.screen = screen
         self.on_play = on_play
         self.enabled = True
         self.font = pygame.font.SysFont(None, 60)
-        self.x = 100
+        self.x_start = 100
         self.x_title = 150
+        self.x_skip = 200
         self.speed = 5
     
     def is_enabled(self):
@@ -25,20 +29,28 @@ class SimpleMenu:
                     sys.exit()
                 if event.key == pygame.K_SPACE:
                     self.on_play()
+                if event.key == pygame.K_j:
+                    self.game.skip_to_boss = True
+                    self.on_play()
 
-        self.x += self.speed
+
+        self.x_start += self.speed
         self.x_title += self.speed
+        self.x_skip += self.speed
         
-        if self.x > 800:
-            self.x = -200
+        if self.x_start > 800:
+            self.x_start = -200
         if self.x_title > 800:
             self.x_title = -400
+        if self.x_skip > 800:
+            self.x_skip = -600
     
     def draw(self):
         self.screen.fill((0, 0, 255))
         
         title = self.font.render("Water Space Platformer", True, (255, 0, 0))
         start = self.font.render("Press SPACE to Play", True, (255, 0, 0))
+        skip = self.font.render("press j to skip to boss", True, (255, 0, 0))
         
         self.screen.blit(title, (self.x_title, 200))
-        self.screen.blit(start, (self.x, 300))
+        self.screen.blit(start, (self.x_start, 300))
